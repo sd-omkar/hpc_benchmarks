08b2808fd9b870e5ce3c8fe847f916435eee4330
sd-omkar <odeshmukh@wisc.edu>
2015-04-22 12:11:37 -0500

    Add DB README

--- db/put.py
+++ db/put.py
@@ -6,8 +6,10 @@ md = MetaData(db)
 hosts = Table('Hosts', md, autoload=True)
 accl = Table('Accelerators', md, autoload=True)
 systems = Table('Systems', md, autoload=True)
+sources = Table('Sources', md, autoload=True)
 
 '''
+# Insert new host
 i = hosts.insert()
 i.execute(hostname = 'lagrange')
 i.execute(hostname = 'euler99')
@@ -15,6 +17,7 @@ i.execute(hostname = 'euler30')
 i.execute(hostname = 'tesla')
 i.execute(hostname = 'kaveri')
 
+# Insert new host
 i = accl.insert()
 i.execute(type='CPU', accl_name='Xeon E5-2690v2', arch='Ivy Bridge-EP', memory=64, cores=40, details='http://ark.intel.com/products/75279/Intel-Xeon-Processor-E5-2690-v2-25M-Cache-3_00-GHz')
 i.execute(type='CPU', accl_name='Core i7-5960X', arch='Haswell-E', memory=32, cores=16, details='http://ark.intel.com/products/82930/Intel-Core-i7-5960X-Processor-Extreme-Edition-20M-Cache-up-to-3_50-GHz')
@@ -25,6 +28,7 @@ i.execute(type='GPU', accl_name='Tesla K40c', arch='Kepler', memory=11.52, cores
 i.execute(type='APU', accl_name='A10-7850K', arch='Kaveri', memory=16, cores=512, details='http://www.amd.com/en-us/products/processors/desktop/a-series-apu')
 i.execute(type='MIC', accl_name='Xeon Phi 5110P', arch='Knights Corner', memory=8, cores=60, details='http://ark.intel.com/products/71992/Intel-Xeon-Phi-Coprocessor-5110P-8GB-1_053-GHz-60-core')
 
+# Insert new host
 i = systems.insert()
 i.execute(OS='Linux', compiler='g++', version=4.81, flags='-O3', library='Blaze', library_ver=2.2, backend='OpenMP', backend_ver=3.1)
 i.execute(OS='Linux', compiler='g++', version=4.81, flags='-O3', library='Blaze', library_ver=2.2, backend='C++', backend_ver=4.81)
@@ -34,4 +38,8 @@ i.execute(OS='Linux', compiler='g++', version=4.81, flags='-O3', library='Thrust
 i.execute(OS='Linux', compiler='icpc', version=14.0, flags='-O3', library='Thrust', library_ver=1.7, backend='OpenMP', backend_ver=3.1)
 i.execute(OS='Linux', compiler='nvcc', version=6.5, flags='-O3', library='Thrust', library_ver=1.7, backend='CUDA', backend_ver=6.5)
 i.execute(OS='Linux', compiler='g++', version=4.81, flags='-O3', library='VexCL', library_ver=1.32, backend='OpenCL', backend_ver=1.2)
+
+# Insert new host
+i = sources.insert()
+i.execute(URL='', commit_hash='')
 '''
